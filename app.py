d9ba69213ad809bb1e3166f9475aa1e0a005ac70
Martin Agoha <martin4dtruth@gmail.com.>
2025-07-15 15:02:44 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+#This is my first python program
+print("i love pizza!")
+print("it's really good!")
+
+#This are strings
+first_name ="samuel"
+food="jellof rice"
+email="samuel3030@gmail.com"
+ 
+print(first_name)
+print(f"Hello{first_name}")
+print(f" i like{food}")
+print(f"my email is: {email}")
+
+#integers
+age = 30
+quantity = 3
+num_of_students= 45
+
+print(f"i am {age} years old")
+print(f"i am buying{quantity} items")
+print(f"There are{num_of_students} students in the class")

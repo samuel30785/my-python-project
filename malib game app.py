41d17a81fc58f6aebe70ac738431afa143608d33
samuel agoha <samuelagoha94@gmail.com>
2025-07-21 14:24:40 +0100

    Add files via upload
    My story game

---
+++ malib game app.py
@@ -0,0 +1,10 @@
+adjective1 = input("enter an adjective (description):")
+noun1 = input ("enter a noun(person, place or thing):")
+adjective2 = input("enter an adjective (description):")
+verb1 = input ("enter a verb ending with 'ing' ")
+adjective3 = input("enter an adjective (description):")
+
+print(f"Today i went to a {adjective1})zoo.")
+print(f"in an exibit i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f" i was {adjective3}!")
